9fb4c0f81b0ba0a9957b02f4e6b393bac5f67c4f
richardier <ianis.richardier@live.fr>
2022-06-13 14:41:58 +0200

    Worked on WarrantyDetailsViewController
    Finished configuring WarrantyDetailsVC's UI. It's now a big table view with custom cells configured accordingly (TopPresentationCell, InvoicePhotoCell, WarrantyDetailsExtraInfoCell).

--- WhereIsMyWarranty/Extensions/BottomBorder.swift
+++ WhereIsMyWarranty/Extensions/BottomBorder.swift
@@ -2,18 +2,7 @@
 //  BottomBorder.swift
 //  WhereIsMyWarranty
 //
-//  Created by Richardier on 30/12/2021.
+//  Created by Richardier on 13/06/2022.
 //
 
-import UIKit
-
-extension UIView {
-  func setBottomBorder() {
-    self.backgroundColor = MWColor.background
-    self.layer.masksToBounds = false
-    self.layer.shadowColor = UIColor.gray.cgColor
-    self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
-    self.layer.shadowOpacity = 1.0
-    self.layer.shadowRadius = 0.0
-  }
-}
+import Foundation
